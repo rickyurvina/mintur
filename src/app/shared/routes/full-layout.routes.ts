9e823e8247863b56b645de5649815dd2f8e0db21
rickyurvina <ricardo.urvina@alumnos.ucn.cl>
2023-01-21 02:40:04 -0300

    Display form and questions in evaluation page

--- src/app/shared/routes/full-layout.routes.ts
+++ src/app/shared/routes/full-layout.routes.ts
@@ -4,5 +4,9 @@ export const FullLayout_ROUTES: Routes = [
     {
         path: 'authentication',
         loadChildren: () => import('../../authentication/authentication.module').then(m => m.AuthenticationModule)
+    },
+    {
+      path:'user',
+      loadChildren:()=>import('../../test-form/test-form.module').then(m=>m.TestFormModule)
     }
 ];
