7371c5a696c6655d611bbad340774b6174e2c480
rickyurvina <ricardo.urvina@alumnos.ucn.cl>
2023-02-14 20:31:25 -0300

    ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  url: "http://localhost:8000/api"
-  // url: "https://api-turismo.laverix.com.ec/api"
+  // url: "http://localhost:8000/api"
+  url: "https://api-turismo.laverix.com.ec/api"
 
 
 };
